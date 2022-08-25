b0e5c3b252159d0965ff8c6b7bb7582f959bed34
SaberTPGO <Pollard.coding@gmail.com>
2022-08-25 10:11:24 -0700

    Changed output from console.log to alert, researching why console.log yielded no result

--- script.js
+++ script.js
@@ -16,5 +16,7 @@ console.log(laterYears)
 myAgeInDogYears = earlyYears+laterYears
 //lower case
 let myName = 'tim'.toLowerCase()
-console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+alert(`My name is ${myName}. I am ${myAge} years old in human years 
+which is ${myAgeInDogYears} years old in dog years.`)
 }
